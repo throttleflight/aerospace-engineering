5dda5d8259783524cfc8dfb7bcd5661140280203
Throttle Flight <airplanenews4@gmail.com>
2024-08-27 13:05:14 -0400

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -48,22 +48,3 @@ export const defaultListPageLayout: PageLayout = {
   ],
   right: [],
 }
-
-// Sort order: folders first, then files. Sort folders and files alphabetically
-Component.Explorer({
-  sortFn: (a, b) => {
-    if ((!a.file && !b.file) || (a.file && b.file)) {
-      // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
-      // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
-      return a.displayName.localeCompare(b.displayName, undefined, {
-        numeric: true,
-        sensitivity: "base",
-      })
-    }
-    if (a.file && !b.file) {
-      return 1
-    } else {
-      return -1
-    }
-  },
-})
