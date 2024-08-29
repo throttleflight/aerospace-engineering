9d507b0241ec96f8e2b671d3d1f5a354594460ec
Throttle Flight <airplanenews4@gmail.com>
2024-08-28 20:37:47 -0400

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,6 +1,39 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
+Component.Explorer({
+  title: "Explorer", // title of the explorer component
+  folderClickBehavior: "collapse", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
+  folderDefaultState: "collapsed", // default state of folders ("collapsed" or "open")
+  useSavedState: true, // whether to use local storage to save "state" (which folders are opened) of explorer
+  mapFn: (node) => {
+    // dont change name of root node
+    if (node.depth > 0) {
+      // set emoji for file/folder
+      if (node.file) {
+        node.displayName = "📄 " + node.displayName
+      } else {
+        node.displayName = "📁 " + node.displayName
+      }
+    }
+  },
+  // Sort order: folders first, then files. Sort folders and files alphabetically
+  sortFn: (a, b) => {
+    if ((!a.file && !b.file) || (a.file && b.file)) {
+      return a.displayName.localeCompare(b.displayName)
+    }
+    if (a.file && !b.file) {
+      return -1
+    } else {
+      return 1
+    }
+  },
+  filterFn: (node) => node.name !== "tags", // filters out 'tags' folder
+  mapFn: undefined,
+  // what order to apply functions in
+  order: ["filter", "map", "sort"],
+})
+
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
@@ -27,39 +60,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(
-      Component.Explorer({
-        title: "Explorer", // title of the explorer component
-        folderClickBehavior: "collapse", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
-        folderDefaultState: "collapsed", // default state of folders ("collapsed" or "open")
-        useSavedState: true, // whether to use local storage to save "state" (which folders are opened) of explorer
-        mapFn: (node) => {
-          // don't change name of root node
-          if (node.depth > 0) {
-            // set emoji for file/folder
-            if (node.file) {
-              node.displayName = "📄 " + node.displayName
-            } else {
-              node.displayName = "📁 " + node.displayName
-            }
-          }
-        },
-        // Sort order: folders first, then files. Sort folders and files alphabetically
-        sortFn: (a, b) => {
-          if ((!a.file && !b.file) || (a.file and b.file)) {
-            return a.displayName.localeCompare(b.displayName)
-          }
-          if (a.file && !b.file) {
-            return -1
-          } else {
-            return 1
-          }
-        },
-        filterFn: (node) => node.name !== "tags", // filters out 'tags' folder
-        // what order to apply functions in
-        order: ["filter", "map", "sort"],
-      })
-    ),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     Component.Graph(),
@@ -68,51 +69,15 @@ export const defaultContentPageLayout: PageLayout = {
   ],
 }
 
-// components for pages that display lists of pages (e.g. tags or folders)
+// components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [
-    Component.Breadcrumbs(),
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-  ],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(
-      Component.Explorer({
-        title: "Explorer", // title of the explorer component
-        folderClickBehavior: "collapse", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
-        folderDefaultState: "collapsed", // default state of folders ("collapsed" or "open")
-        useSavedState: true, // whether to use local storage to save "state" (which folders are opened) of explorer
-        mapFn: (node) => {
-          // don't change name of root node
-          if (node.depth > 0) {
-            // set emoji for file/folder
-            if (node.file) {
-              node.displayName = "📄 " + node.displayName
-            } else {
-              node.displayName = "📁 " + node.displayName
-            }
-          }
-        },
-        // Sort order: folders first, then files. Sort folders and files alphabetically
-        sortFn: (a, b) => {
-          if ((!a.file && !b.file) || (a.file and b.file)) {
-            return a.displayName.localeCompare(b.displayName)
-          }
-          if (a.file && !b.file) {
-            return -1
-          } else {
-            return 1
-          }
-        },
-        filterFn: (node) => node.name !== "tags", // filters out 'tags' folder
-        // what order to apply functions in
-        order: ["filter", "map", "sort"],
-      })
-    ),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
 }
